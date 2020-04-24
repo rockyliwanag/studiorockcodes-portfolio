1ac7498d2c82306217f5f740fde16f522978b3ed
Rocky Liwanag <studiorockprod@me.com>
2020-04-24 10:01:42 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-54a2915025b730885a0d.js"
+    "url": "webpack-runtime-b46f3b45e5aa6401d37d.js"
   },
   {
     "url": "framework-38299d20c7ca7a3581d4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "20957db51536e54b07edceaceb28adda"
+    "revision": "1bdaf5fb575760aec94e68edcd166cbf"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "db22d5c87b2e9988a788451c7e6a725f"
+    "revision": "3107a38422459251abd0cfb90c32fb2a"
   },
   {
     "url": "manifest.webmanifest",

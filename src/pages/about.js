7ef1c40cd53927bf23fe64a1eb5413af31a94dbd
Rocky Liwanag <studiorockprod@me.com>
2020-04-30 14:09:34 -0700

    quick changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,16 +21,18 @@ export default () => {
             I am Rocky , a graphic designer turned software developer. Like most
             creative developers, it was a natural progression to go from being a
             graphic artist to learning how to code. I have always had the
-            interest, and was an early adopter of technology. My era was in the
-            days of NES, Powerglove, and playing Track n Field on a Power Pad.
+            interest, and was an early adopter of technology.
+            {/*My era was in the
+            days of NES, Powerglove, and playing Track n Field on a Power Pad. */}
             My first exposure to pixels were on an MS-Paint program, pre Windows
             95 version.
             <br />
             <br />
             Finishing a Bachelor's degree in Marketing/Advertising at APC,
-            Philippines; I learned how valuable design is. It has fascinated me
-            how effective a good design can get, targeted at the right audience.
-            And how local mom and pop brands break into becoming global brands.
+            Philippines; I learned how valuable design is. It has always
+            fascinated me how effective a good design can get, targeted at the
+            right audience; And how local mom and pop brands break into becoming
+            global brands.
             <br />
             <br />
             Having these in mind, and building my technical skillset, my goal is

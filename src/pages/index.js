e0322a154d0bee5f350ee0b04a8433aee7b8c6b9
Rocky Liwanag <studiorockprod@me.com>
2021-08-05 10:31:10 -0700

    fix netlify form

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,11 +146,10 @@ const HomeIndex = () => {
                       data-netlify="true"
                       action="/"
                     >
-                      <input type="hidden" name="bot-field" />
-<input type="hidden" name="form-name" value="contact" />
                       <div className="row uniform 50%">
                         <div className="6u 12u$(xsmall)">
-                          <input type="hidden" name="bot-field" value="contact" />
+                        <input type="hidden" name="bot-field" />
+                          <input type="hidden" name="contact" value="contact" />
                           <input
                             type="text"
                             name="name"
